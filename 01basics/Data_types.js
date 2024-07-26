cc813aa2f60f29c58dcdb44e045f2e18ed2860c8
garud-1 <142133577+garud-1@users.noreply.github.com>
2024-07-26 15:17:57 +0000

    added new data and files

---
+++ 01basics/Data_types.js
@@ -0,0 +1,20 @@
+ 
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null < 0);
+console.log(null <= 0);
+console.log(null == 0);
+
+/*
+false
+true
+false
+true
+false
+
+this occurs because 
+the comparison oerator and  equality operator act differently
+the comparison operator converts null to 0
+ 
+*/
---
+++ functions/Function_01.js
@@ -0,0 +1,20 @@
+// sayHello();
+
+// function sayHello() {
+//   console.log("Hello");
+// }
+
+if (2 === "2") {
+    console.log("This is true");
+  }
+  
+  var myName = "hitesh";
+  if (myName === global.myName) {
+    console.log("This is again a true statement");
+  }
+  
+  console.log("Hello");
+  console.log(-true);
+ 
+
+  
