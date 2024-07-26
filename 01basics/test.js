b90c87d94af480104abab2c80273764f0b2f063c
garud-1 <142133577+garud-1@users.noreply.github.com>
2024-07-26 13:51:56 +0000

    starting javascript

---
+++ 01basics/test.js
@@ -0,0 +1,25 @@
+// 
+var fullName = "Akshay Wale";
+
+// how to name variables
+
+// fullName
+// full-name
+// full_name
+// FullName
+
+var courseName = "JS";
+
+var isLoggedIn = false;
+
+var loggedCount = 34;
+
+// console.log(loggedCount);
+// console.log("loggedCount");
+
+var paymentMode;
+
+paymentMode = "Credit Card";
+console.log(paymentMode);
+
+// var if = 23;
